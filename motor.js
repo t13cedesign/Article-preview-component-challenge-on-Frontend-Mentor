5d96d545b66c0e18c0c7b00737161f0f79991fd9
NeXuS_13 <t13cedesign.com>
2021-07-26 19:28:17 +0200

    upload project

---
+++ motor.js
@@ -0,0 +1,17 @@
+const shareButton = document.getElementById('share');
+const shareIcon = document.getElementById('shareImg')
+const shareButtonShareC = document.getElementById('share-r')
+const shareContainer = document.querySelector('.share-container');
+
+function showShareContainer() {
+    shareContainer.classList.toggle('share-container--active');
+    shareButton.classList.toggle('share-icon--active');
+    shareIcon.classList.toggle('share-img--active')
+}
+
+// function showShareContainer() {
+//     shareContainer.style.display = "flex";
+// }
+
+shareButton.addEventListener('click', showShareContainer);
+shareButtonShareC.addEventListener('click', showShareContainer);
